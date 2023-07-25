85e1e882f6c94810653569f4f4c5139a418e0ff7
VictorApaez <vic.apaez@gmail.com>
2023-07-25 19:22:01 -0400

    updated test for customer class and created class

--- customer.py
+++ customer.py
@@ -1,3 +1,34 @@
+from bank import Bank
 class Customer:
-    def __init__(self, name, address):
-        pass
+    def __init__(self, name, address, bank):
+        self.name = name
+        self.address = address
+        self.bank = bank
+        self.accounts = []
+
+    def open_account(self, account_number, initial_balance):
+        account = self.bank.create_account(account_number, initial_balance)
+        self.accounts.append(account)
+        return account
+
+    def close_account(self, account_number):
+        for account in self.accounts:
+            if account.account_number == account_number:
+                self.bank.delete_account(account_number)
+                self.accounts.remove(account)
+                break
+        else:
+            raise ValueError(f"No account with number {account_number} found.")
+
+    def transfer_money(self, from_account_number, to_account_number, amount):
+        from_account = self._find_account(from_account_number)
+        to_account = self._find_account(to_account_number)
+
+        from_account.withdraw(amount)
+        to_account.deposit(amount)
+
+    def _find_account(self, account_number):
+        for account in self.accounts:
+            if account.account_number == account_number:
+                return account
+        raise ValueError(f"No account with number {account_number} found.")
--- tests/test_customer.py
+++ tests/test_customer.py
@@ -3,31 +3,33 @@ from account import Account
 from customer import Customer
 
 def test_customer_creation():
-    customer = Customer('John Doe', '123 Main St')
+    bank = Bank('Test Bank')
+    customer = Customer('John Doe', '123 Main St', bank)
     assert customer.name == 'John Doe'
     assert customer.address == '123 Main St'
     assert len(customer.accounts) == 0
 
 def test_open_account():
     bank = Bank('Test Bank')
-    customer = Customer('John Doe', '123 Main St')
-    account = customer.open_account(bank, '123456', 100)
+    customer = Customer('John Doe', '123 Main St', bank)
+    account = customer.open_account('123456', 100)
     assert len(customer.accounts) == 1
     assert isinstance(account, Account)
     assert account in customer.accounts
 
 def test_close_account():
     bank = Bank('Test Bank')
-    customer = Customer('John Doe', '123 Main St')
-    customer.open_account(bank, '123456', 100)
+    customer = Customer('John Doe', '123 Main St', bank)
+    account = customer.open_account('123456', 100)
+    account.withdraw(100)
     customer.close_account('123456')
     assert len(customer.accounts) == 0
 
 def test_transfer_money():
     bank = Bank('Test Bank')
-    customer = Customer('John Doe', '123 Main St')
-    account1 = customer.open_account(bank, '123456', 100)
-    account2 = customer.open_account(bank, '789012', 50)
+    customer = Customer('John Doe', '123 Main St', bank)
+    account1 = customer.open_account('123456', 100)
+    account2 = customer.open_account('789012', 50)
     customer.transfer_money('123456', '789012', 50)
     assert account1.balance == 50
     assert account2.balance == 100
